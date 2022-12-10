c8bbea5e661d29a540bf6114d465d93c2cf1200b
gaatvul <kungfuscience@hotmail.com>
2022-12-10 21:30:09 +0000

    pom.xml - Added dependency for Spring Security.
    application.properties
    - Enabled web level logging for developement and testing purposes.
    
    AppConfig.java
    - Added bean for userAccountDAOImpl
    
    SecurityConfig.java
    - Added security configuration class for Spring Security.
    - Added bean for URI authorisation.
    - Added bean for authenication manager.
    - Added bean for password encoder.

--- src/main/java/com/gaatvul/bugtracker/configurations/AppConfig.java
+++ src/main/java/com/gaatvul/bugtracker/configurations/AppConfig.java
@@ -1,16 +1,23 @@
 package com.gaatvul.bugtracker.configurations;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.gaatvul.bugtracker.DAOs.BugReportDAO;
 import com.gaatvul.bugtracker.DAOs.BugReportDAOImpl;
+import com.gaatvul.bugtracker.DAOs.UserDetailsDAOImpl;
 import com.gaatvul.bugtracker.services.BugReportService;
 import com.gaatvul.bugtracker.services.BugReportServiceImpl;
 
 @ComponentScan("com.gaatvul.bugtracker")
 public class AppConfig {
+
+    @Autowired
+    DataSource dataSource;
     
     @Bean(name = "bugReportServiceImpl")
     public BugReportService creatBugReportService() {
@@ -25,5 +32,10 @@ public class AppConfig {
     @Bean(name = "jdbcTemplate")
     public JdbcTemplate createJdbcTemplate() {
         return new JdbcTemplate();
+    }
+
+    @Bean(name = "userAccountDAOImpl")
+    public UserDetailsDAOImpl createUserAccountDAO() {
+        return new UserDetailsDAOImpl();
     }
 }
---
+++ src/main/java/com/gaatvul/bugtracker/configurations/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.gaatvul.bugtracker.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.gaatvul.bugtracker.services.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+        .authorizeHttpRequests(authorize -> authorize
+        .antMatchers("/signup", "/css/**", "/js/**", "/webjars/**").permitAll()
+        .antMatchers("/**").authenticated())
+        .formLogin();
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager buildAuthenticationManager(HttpSecurity http, UserDetailsServiceImpl userDetailsService)
+            throws Exception {
+
+        return http
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+
+        return NoOpPasswordEncoder.getInstance();
+    }
+}

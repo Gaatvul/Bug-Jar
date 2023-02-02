055e44aca3fd88f1518f8c788d7223bc6004b681
gaatvul <kungfuscience@hotmail.com>
2023-02-02 17:25:42 +0000

    Added button for sending user to their dashboard. This button only appears when a user is logged in.
    Added logic to hide the sign up. log in and demo accounts buttons if a user is signed in.
    
    Minor refactoring

--- src/main/java/com/gaatvul/bugtracker/controllers/IndexController.java
+++ src/main/java/com/gaatvul/bugtracker/controllers/IndexController.java
@@ -1,14 +1,16 @@
 package com.gaatvul.bugtracker.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping(value = "/")
-    public String getIndex() {
-
+    public String getIndex(Authentication auth, Model model) {
+        model.addAttribute("userIsLoggedIn", (auth==null));
         return "index";
     }
 
